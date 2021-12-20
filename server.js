1a1170c1be019ec47299da8152a7d384736877d4
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-12-20 22:58:56 +0100

    test

--- server.js
+++ server.js
@@ -15,7 +15,21 @@ const corsOptions ={
     optionSuccessStatus:200,
  }
  
-app.use(cors(corsOptions)) 
+ app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
 let counter = 0;
 io.addServer(server);
