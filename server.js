a4cc9ccd38a93076b2e43ec38b22ea7d91c9b1e5
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-12-20 22:42:17 +0100

    cors 2

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import geckos from '@geckos.io/server';
 import http from 'http'
 import express from 'express'
 import _ from 'lodash';
+import cors from 'cors';
 
 const PORT = 8081;
 const app = express();
 const server = http.createServer(app);
 const io = geckos();
-const cors=require("cors");
 const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
